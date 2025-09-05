cc168ed86fb70a443b226be5bb81d24c234ae1ff
Wojciech Kania <wojtek@kania.sh>
2025-09-05 14:50:37 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-full justify-center relative bg-black text-white flex items-center">
-      <div className="font-brockmann text-2xl">Next design summit</div>
+      <div className="font-brockmann text-2xl">Next design summitxs</div>
     </div>
   );
 }
